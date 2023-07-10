0ceb8318b4c42f580dedfc0ec83ee241041d9904
ArtanFS <art_anatoliy@ukr.net>
2023-07-11 01:08:04 +0300

    make icon transform

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,19 +1,18 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
+  const mobileMenuBtn = document.querySelector('.js-show-btn');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-  const closeMenuOpenBasket = document.querySelector('.js-closemenu-openbasket');
-  const closeMenuLinkA = document.querySelector('.js-close-linka');
-  const closeMenuLinkB = document.querySelector('.js-close-linkb');
-  const closeMenuLinkC = document.querySelector('.js-close-linkc');
-  const openMenuBtn2 = document.querySelector('.js-open-menu2');
+  const closeMenuBtnA = document.querySelector('.js-close-menua');
+  const closeMenuBtnB = document.querySelector('.js-close-menub');
+  const closeMenuBtnC = document.querySelector('.js-close-menuc');
+  const closeMenuBtnD = document.querySelector('.js-close-menud');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
-      openMenuBtn2.classList.toggle('show');
+      mobileMenuBtn.classList.toggle('show');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -22,11 +21,10 @@
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuLinkA.addEventListener('click', toggleMenu);
-  closeMenuLinkB.addEventListener('click', toggleMenu);
-  closeMenuLinkC.addEventListener('click', toggleMenu);
-  closeMenuOpenBasket.addEventListener('click', toggleMenu);
+  closeMenuBtnA.addEventListener('click', toggleMenu);
+  closeMenuBtnB.addEventListener('click', toggleMenu);
+  closeMenuBtnC.addEventListener('click', toggleMenu);
+  closeMenuBtnD.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
