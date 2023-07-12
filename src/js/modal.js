a8fa2de43482a81db96166f8a24c3942cb001ba9
ArtanFS <art_anatoliy@ukr.net>
2023-07-12 17:39:45 +0300

    add open modal header

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtnHeader: document.querySelector("[data-modalopen-header]"),
+    // openModalBtn: document.querySelector("[data-modal-open]"),
+    // openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnHeader.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
