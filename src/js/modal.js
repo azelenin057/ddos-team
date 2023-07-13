5bae0b194d0363c48a6b8aceefed51adb0f9fe89
ArtanFS <art_anatoliy@ukr.net>
2023-07-13 12:11:13 +0300

    fix js
    modal btn vegetables
    scroll lock  on modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     openModalBtnHeader: document.querySelector("[data-modalopen-header]"),
     openModalBtnMenu: document.querySelector("[data-modalopen-menu]"),
+    openModalBtnVegetables: document.querySelector("[data-modalopen-vegetables]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
@@ -10,6 +11,7 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.openModalBtnHeader.addEventListener("click", toggleModal);
   refs.openModalBtnMenu.addEventListener("click", toggleModal);
+  refs.openModalBtnVegetables.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
